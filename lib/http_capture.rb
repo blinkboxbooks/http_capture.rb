c22547d3d71b77a2e903f54d14dfe116b9f276b0
Greg Beech <greg@blinkbox.com>
2013-07-08 18:23:34 +0100

    Added a MockRequest class with setters for properties.

--- lib/http_capture.rb
+++ lib/http_capture.rb
@@ -4,6 +4,8 @@ module HttpCapture
   RESPONSES = []
 
   class Response
+    include Enumerable
+
     def initialize(real_response)
       @real_response = real_response
     end
@@ -11,6 +13,16 @@ module HttpCapture
     # The default header accessor
     def [](key)
       @real_response[key]
+    end
+
+    def each(&block)
+      if @real_response.respond_to?(:header)
+        @real_response.header.each(&block)
+      elsif @real_response.respond_to?(:headers)
+        @real_response.headers.each(&block)
+      else
+        @real_response.each(&block)
+      end
     end
 
     # The default status code accessor
@@ -21,6 +33,31 @@ module HttpCapture
     # The default body accessor
     def body
       @real_response.body
+    end
+  end
+
+  class MockResponse
+    include Enumerable
+
+    attr_accessor :status
+    attr_accessor :body
+
+    def initialize
+      @header = {}
+    end
+
+    # The default header accessor
+    def [](key)
+      @header[key]
+    end
+
+    # The default header accessor
+    def []=(key, value)
+      @header[key] = value
+    end
+
+    def each(&block)
+      @header.each(&block)
     end
   end
 end

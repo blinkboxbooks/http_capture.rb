b6f443f54e1cc6d61ab8ba590ac09c96ac9b3cad
Greg Beech <greg@blinkbox.com>
2013-12-09 14:33:58 +0000

    Captures request and duration
    Now supports basic capturing of the request, and the duration that it
    took to get the response, which is useful for adding ad-hoc performance
    metrics to tests.

--- spec/capture_spec.rb
+++ spec/capture_spec.rb
@@ -1,44 +1,68 @@
 require 'net/http/capture'
 
 shared_examples "capturing HTTP responses" do
-    before :each do
-        # Clear the captured requests
-        HttpCapture::RESPONSES.clear
+  before :each do
+    # Clear the captured requests
+    HttpCapture::RESPONSES.clear
+  end
+
+  it 'should capture a HTTP GET request' do
+    # Do the request
+    perform_get(make_test_uri())
+
+    # Really I should check that it has exactly 1 item, but for now we're happy that something comes through.
+    HttpCapture::RESPONSES.length.should > 0
+  end
+
+  context 'last captured response wrapper' do
+    before :all do
+      HttpCapture::RESPONSES.clear
+      perform_get(make_test_uri(200, "text"))
+      @it = HttpCapture::RESPONSES.last
+    end
+    
+    it 'should capture the response that made the request' do
+      expect(@it.request).to_not be_nil
+    end
+    
+    it 'should record the time it took to get the response with the #duration method' do
+      expect(@it.duration).to be > 0
+    end
+    
+    it 'should return headers with the #[](key) method' do
+      expect(@it['Content-Length']).to be_a_kind_of(String)
     end
 
-    it 'should capture a Net::HTTP.get request' do
-        # Do the request
-        perform_get(make_test_uri())
-
-        # Really I should check that it has exactly 1 item, but for now we're happy that something comes through.
-        HttpCapture::RESPONSES.length.should > 0
+    it 'should support enumerating the headers with the #each method' do
+      @it.each { |item| expect(item).to be_a_kind_of(Array) }
     end
 
-    context 'last captured response wrapper' do
-        before :all do
-            HttpCapture::RESPONSES.clear
-
-            @actual = perform_get(make_test_uri(200,"text"))
-
-            @res = HttpCapture::RESPONSES.last
-        end
-        
-        it 'should return headers with the #[](key) method' do
-            @res['Content-Length'].should be_a(String)
-        end
-
-        it 'should support enumerating the headers method' do
-            @res.each do |item|
-                item.should be_a(Array)
-            end
-        end
-
-        it 'should return the HTTP status with the #status method' do
-            @res.status.should == 200
-        end
-
-        it 'should return the HTTP body with the #body method' do
-            @res.body.should == "text"
-        end
+    it 'should return the status with the #status method' do
+      expect(@it.status).to eq(200)
     end
+
+    it 'should return the body with the #body method' do
+      expect(@it.body).to eq("text")
+    end
+  end
+
+  context('last captured request wrapper') do
+    before :all do
+      HttpCapture::RESPONSES.clear
+      perform_get(make_test_uri(200, "text"))
+      @it = HttpCapture::RESPONSES.last.request
+    end
+
+    it 'should return the method with the #method method' do
+      expect(@it.method).to eq("GET")
+    end
+
+    it 'should return the path with the #path method' do
+      expect(@it.path).to_not be_nil # TODO: This should be a stronger check...
+    end
+
+    it 'should return the full URI with the #uri method' do
+      expect(@it.uri).to be_a_kind_of(URI)
+    end
+  end  
 end
